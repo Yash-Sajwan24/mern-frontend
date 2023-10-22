08d1a72c27a0bc0d67976fb43c48cbadb4cf08a7
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-10-23 04:15:23 +0530

    sign up onchnage

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,26 +1,83 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Signup = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    phone: "",
+  });
+
+  let name, value;
+  const handleInput = (e) => {
+   
+    name = e.target.ariaLabel;
+    value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]: value
+    });
+  };
   return (
     <>
-    <form > 
-    <div className="col" >
-    <input type="text" className="form-control w-25 mx-auto mt-5 " placeholder="Name" aria-label="Name" />
-    <input type="email" className="form-control w-25 mx-auto mt-3 " placeholder="Email" aria-label="Email" />
-    <input type="password" className="form-control w-25 mx-auto mt-3 " placeholder="Password" aria-label="Password" />
-    <input type="number" className="form-control w-25 mx-auto mt-3 " placeholder="Phone" aria-label="Phone" />
-    <button style={{  backgroundColor: "yellow", display: "flex", alignItems: "center", justifyContent: "center", color: "white"}} type='submit' className='w-25 mx-auto mx-auto mt-3 bg-dark'> Register</button>
-    </div>
-    </form>
-    
-    <div style={{textAlign: "center", marginTop: "50px"}}> 
-         <NavLink to="/login" > I am already Registered</NavLink>
-    </div>
-    
- 
-    </>
-  )
-}
+      <form>
+        <div className="col">
+          <input
+            type="text"
+            className="form-control w-25 mx-auto mt-5 "
+            placeholder="Name"
+            aria-label="name"
+            value={user.name}
+            onChange={handleInput}
+          />
+          <input
+            type="email"
+            className="form-control w-25 mx-auto mt-3 "
+            placeholder="Email"
+            aria-label="email"
+            value={user.email}
+            onChange={handleInput}
+          />
+          <input
+            type="password"
+            className="form-control w-25 mx-auto mt-3 "
+            placeholder="Password"
+            aria-label="password"
+            value={user.password}
+            onChange={handleInput}
+          />
+          <input
+            type="number"
+            className="form-control w-25 mx-auto mt-3 "
+            placeholder="Phone"
+            aria-label="phone"
+            value={user.phone}
+            onChange={handleInput}
+          />
+          <button
+            style={{
+              backgroundColor: "yellow",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              color: "white",
+            }}
+            type="submit"
+            className="w-25 mx-auto mx-auto mt-3 bg-dark"
+          >
+            {" "}
+            Register
+          </button>
+        </div>
+      </form>
 
-export default Signup
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <NavLink to="/login"> I am already Registered</NavLink>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
