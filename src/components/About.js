945553dca4515081ed7dd36b5437ff6ec1cc0893
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-10-27 16:07:02 +0530

    jit authentication added

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { useNavigate } from "react-router-dom";
+
 
 const About = () => {
+  const navigate = useNavigate();
+
+  const [data, setData] = useState({
+    name : '', 
+    email: '', 
+    phone : '',
+  });
+  
+
+  const callAboutUsPage = async () => {
+    try{
+
+      const res = await fetch("/about", {
+        method: "GET", 
+        headers : {
+          Accept: 'application/json', 
+          "Content-Type" : "application/json",
+        }, 
+        credentials : "include",
+      });
+
+      let data = await res.json();
+      if(res.error){
+        throw new Error(res.error);
+      }
+      setData(data);
+      
+    }
+    catch(err){
+      navigate('../login');
+    }
+  }
+
+  useEffect(()=> {
+    callAboutUsPage();
+  }, []);
+
   return (
     <div
       style={{
@@ -18,9 +57,9 @@ const About = () => {
         fontSize : "20px"
       }}
     >
-        <h3>Name</h3>  Yash Sajwan <br></br><br></br>
-        <h3>Email</h3>  yashsajwan@gmail.com<br></br><br></br>
-        <h3>Phone</h3>  0293840382049<br></br><br></br>
+        <h3>Name</h3>  {data.name} <br></br><br></br>
+        <h3>Email</h3>  {data.email}<br></br><br></br>
+        <h3>Phone</h3>  {data.phone}<br></br><br></br>
 
     </div>
   );
